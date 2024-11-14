ad2cc2f822fbb9b2807d238ac0ebf3465399d433
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2024-11-14 14:14:10 -0300

    :construction: Header em construção

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,47 @@
-import { Link } from "react-router-dom";
+import { Link, useMatch } from "react-router-dom";
 import Logo from "../../assets/img/logo.png";
 import { FaSearch } from "react-icons/fa";
+import S from "./style.module.scss";
 
 export default function Home() {
+  const matchHome = useMatch("/");
+  const matchDonatedBooks = useMatch("/donatedBooks");
+  const matchToDonatedBooks = useMatch("/ToDonatedBooks");
+
   return (
     <>
-      <header>
-        <section>
+      <header className={S}>
+        <section className={S["title-container"]}>
           <img src={Logo} alt="" />
           <h1>Livros Vai na Web</h1>
         </section>
-        <nav>
-          <Link to="/">Início</Link>
-          <Link to="/donatedBooks">Livros doados</Link>
-          <Link to="/ToDonatedBooks">Doe seu livro</Link>
+        <nav className={S["nav-container"]}>
+          <Link
+            to="/"
+            className={`${S["nav-link"]} ${matchHome ? S.active : ""}`}
+          >
+            Início
+          </Link>
+          <Link
+            to="/donatedBooks"
+            className={`${S["nav-link"]} ${matchDonatedBooks ? S.active : ""}`}
+          >
+            Livros doados
+          </Link>
+          <Link
+            to="/ToDonatedBooks"
+            className={`${S["nav-link"]} ${
+              matchToDonatedBooks ? S.active : ""
+            }`}
+          >
+            Doe seu livro
+          </Link>
         </nav>
+        <section>
+          <input name="search" type="text" placeholder="O que você procura?" />
+          <FaSearch />
+        </section>
       </header>
-      <section>
-        <input name="search" type="text" placeholder="O que você procura?" />
-        <FaSearch />
-      </section>
     </>
   );
 }
