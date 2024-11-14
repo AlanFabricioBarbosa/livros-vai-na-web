cf1269fca2138af3ab80090aa28b612e50cedad4
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2024-11-14 08:58:37 -0300

    :construction: Começando a construir a header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
+import Logo from "../../assets/img/logo.png";
+import { FaSearch } from "react-icons/fa";
 
 export default function Home() {
   return (
     <>
       <header>
+        <section>
+          <img src={Logo} alt="" />
+          <h1>Livros Vai na Web</h1>
+        </section>
         <nav>
-          <Link to="/">Home</Link>
+          <Link to="/">Início</Link>
           <Link to="/donatedBooks">Livros doados</Link>
           <Link to="/ToDonatedBooks">Doe seu livro</Link>
         </nav>
       </header>
+      <section>
+        <input name="search" type="text" placeholder="O que você procura?" />
+        <FaSearch />
+      </section>
     </>
   );
 }
