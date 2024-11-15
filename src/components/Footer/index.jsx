6db1fdd917e432781fcdd07a5ae115cf07321577
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2024-11-15 16:45:50 -0300

    :construction: Footer em construção

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,67 @@
+import { FaFacebook, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
+import { IoLogoLinkedin } from "react-icons/io";
+
 export default function Footer() {
   return (
-    <>
-      <footer>
-        <p>Links para contato</p>
-      </footer>
-    </>
+    <footer>
+      <section>
+        <p>4002 - 8922</p>
+        <ul>
+          <li>
+            <a
+              href="https://pt-br.facebook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Facebook"
+            >
+              <FaFacebook />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://x.com/home"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Twitter"
+            >
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="YouTube"
+            >
+              <FaYoutube />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/feed/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn"
+            >
+              <IoLogoLinkedin />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Instagram"
+            >
+              <FaInstagram />
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024</p>
+      </section>
+    </footer>
   );
 }
