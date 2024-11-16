c37dfc4da5d149b12f394348ce3ee5bd56dc0fe7
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2024-11-16 14:13:08 -0300

    :sparkles: feat: Component banner da home

---
+++ src/components/BannerHome/index.jsx
@@ -0,0 +1,9 @@
+import S from "./style.module.scss";
+
+export default function BannerHome() {
+  return (
+    <section className={S["banner-container"]}>
+      <p>Venha fazer parte da maior rede de doação</p>
+    </section>
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,9 @@
+import BannerHome from "../../components/BannerHome";
+
 export default function Home() {
   return (
     <>
-      <h2>Home</h2>
-      <p>Criando rotas</p>
+      <BannerHome />
     </>
   );
 }
