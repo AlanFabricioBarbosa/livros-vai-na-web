e1f89750fcfaf3aa92727936100c468f8b6a3aee
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2024-11-23 15:47:51 -0300

    :sparkles: feat: Texto na pagina de livros doados

--- src/pages/DonatedBooks/index.jsx
+++ src/pages/DonatedBooks/index.jsx
@@ -1,8 +1,10 @@
 import CardBooks from "../../components/CardBooks";
+import S from "./style.module.scss";
 
 export default function DonatedBooks() {
   return (
     <>
+      <p className={S["text-donated-books"]}>Livros Doados</p>
       <CardBooks />
     </>
   );
