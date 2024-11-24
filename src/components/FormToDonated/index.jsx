50b43dc80247ffc52d5f2c1a516392e66f30b5ac
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2024-11-24 16:03:24 -0300

    :sparkles: feat: Página para doar livros

---
+++ src/components/FormToDonated/index.jsx
@@ -0,0 +1,53 @@
+import S from "./style.module.scss";
+import FormBook from "../../assets/img/formBook.png";
+
+export default function FormToDonated() {
+  return (
+    <section className={S["form-container"]}>
+      <form action="">
+        <fieldset>
+          <img src={FormBook} alt="" />
+          <legend>Informações do Livro</legend>
+
+          <label htmlFor="title">Titulo:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Insira o título do livro"
+            required
+          />
+
+          <label htmlFor="genre">Gênero:</label>
+          <input
+            type="text"
+            id="genre"
+            name="genre"
+            placeholder="Ex.: Romance, Ficção"
+            required
+          />
+
+          <label htmlFor="author">Autor{"(a)"}:</label>
+          <input
+            type="text"
+            id="author"
+            name="author"
+            placeholder="Nome do autor(a)"
+            required
+          />
+
+          <label htmlFor="urlImg">Link da imagem:</label>
+          <input
+            type="url"
+            id="urlImg"
+            name="urlImg"
+            placeholder="https://exemplo.com/imagem.jpg"
+            required
+          />
+
+          <button type="submit">Doar</button>
+        </fieldset>
+      </form>
+    </section>
+  );
+}
--- src/pages/ToDonatedBooks/index.jsx
+++ src/pages/ToDonatedBooks/index.jsx
@@ -1,8 +1,18 @@
+import FormToDonated from "../../components/FormToDonated";
+import S from "./style.module.scss";
+
 export default function ToDonatedBooks() {
   return (
     <>
-      <h2>Doe seu livro</h2>
-      <p>Criando rotas</p>
+      <section>
+        <div>
+          <p className={S["text-to-donated-container"]}>
+            Por favor, preencha o formulário com suas informações e as
+            informações do Livro
+          </p>
+        </div>
+        <FormToDonated />
+      </section>
     </>
   );
 }
